29f4cdc033a7a4f2a207c3d90fc16273bc12e9cc
perrydrums <psperryjanssen@gmail.com>
2017-10-23 13:19:50 +0200

    feat(CMS): Party/Artist show view

--- resources/views/artists/show.blade.php
+++ resources/views/artists/show.blade.php
@@ -8,32 +8,37 @@
             <h1>{{ $artist['name'] }}</h1>
             <hr>
 
-            {{--
-                todo: Only display if data is set
-                todo: Forearch loop through content
-             --}}
-
             <table width="100%">
+                @if(isset($artist['firstname']))
                 <tr>
                     <td width="50%">Full name</td>
                     <td>{{ $artist['firstname'] . ' ' . $artist['lastname'] }}</td>
                 </tr>
+                @endif
+                @if(isset($artist['alias']))
                 <tr>
                     <td width="50%">Alias</td>
                     <td>{{ $artist['alias'] }}</td>
                 </tr>
+                @endif
+                @if(isset($artist['website']))
                 <tr>
                     <td width="50%">Website</td>
                     <td><a href="{{ $artist['website'] }}">{{ $artist['website'] }}</a></td>
                 </tr>
+                @endif
+                @if(isset($artist['spotify']))
                 <tr>
                     <td width="50%">Spotify</td>
                     <td><a href="{{ $artist['spotify'] }}">{{ $artist['spotify'] }}</a></td>
                 </tr>
+                @endif
+                @if(isset($artist['soundcloud']))
                 <tr>
                     <td width="50%">Soundcloud</td>
                     <td><a href="{{ $artist['soundcloud'] }}">{{ $artist['soundcloud'] }}</a></td>
                 </tr>
+                @endif
             </table>
 
         </div>
--- resources/views/parties/show.blade.php
+++ resources/views/parties/show.blade.php
@@ -7,31 +7,32 @@
         <div class="col-md-6 col-md-offset-3">
             <h1>{{ $party['name'] }}</h1>
             <hr>
-
-            {{--
-                todo: Only display if data is set
-                todo: Forearch loop through content
-             --}}
-
             <table width="100%">
-                <tr>
-                    <td width="50%">Organizer</td>
-                    <td>{{ $party['organizer'] }}</td>
-                </tr>
+                @if(isset($party['organizer']))
+                    <tr>
+                        <td width="50%">Organizer</td>
+                        <td>{{ $party['organizer'] }}</td>
+                    </tr>
+                @endif
+                @if(isset($party['location']))
                 <tr>
                     <td width="50%">Location</td>
                     <td>{{ $party['location'] }}</td>
                 </tr>
+                @endif
+                @if(isset($party['date']))
                 <tr>
                     <td width="50%">Date</td>
                     <td>{{ $party['date'] . ' at ' . $party['time']}}</td>
                 </tr>
+                @endif
+                @if(isset($party['picture']))
                 <tr>
                     <td width="50%">Picture path</td>
                     <td>{{ $party['picture'] }}</td>
                 </tr>
+                @endif
             </table>
-
         </div>
     </div>
 
